ae4dac8eadd44e07c81698a4f5b812c092572bf2
makit <info@makit.net>
2022-09-03 09:31:09 +0100

    Initial commit

---
+++ test/aws-twitter-mentions-bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsTwitterMentionsBot from '../lib/aws-twitter-mentions-bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-twitter-mentions-bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsTwitterMentionsBot.AwsTwitterMentionsBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
